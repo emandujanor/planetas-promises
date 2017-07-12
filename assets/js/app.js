8dc6fd6542acb12a5b736c98b0026478c4214c5f
Edith Mandujano <edithmandujano001@gmail.com>
2017-07-12 16:47:56 -0500

    inicio

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+//console.log ("listo");
+var ajax = new XMLHttpRequest();
+var url = data/earth-like-results.json
+ajax.open("GET", url);
+ajax.send();
+
+ajax.onreadystatechange= function(){
+  
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.use('/static', express.static(path.join(__dirname,'node_modules')));
+app.use('/data', express.static(path.join(__dirname,'/data')));
+app.use('/static', express.static(path.join(__dirname,'/assets')));
+
+app.listen(2500);
